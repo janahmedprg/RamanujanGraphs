03a3cf330f8ec93bf715ab2b105e2f5ed3f86b4d
Jan Ahmed <jan.ahmed.prg@gmail.com>
2022-07-10 14:30:38 -0400

    need to debug

---
+++ checkIndependentSet.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> getNeighbors(vector<int> adjacencyList)
+{
+    vector<int> neighbors;
+    int n = adjacencyList.size();
+    for (int i = 0; i < n; ++i)
+    {
+        if (adjacencyList[i] == 1)
+        {
+            neighbors.push_back(i);
+        }
+    }
+    return neighbors;
+}
+
+int main()
+{
+    srand(time(0));
+    int n;
+    cout << "Enter number of vertices"
+         << "\n";
+    cin >> n;
+    vector<vector<int>> A;
+    A.resize(n, vector<int>(n));
+    ifstream inFile("lpsP3Q13.txt");
+    int a, b;
+    inFile >> a >> b;
+    while (a != -1 && b != -1)
+    {
+        A[a - 1][b - 1] = 1;
+        A[b - 1][a - 1] = 1;
+        inFile >> a >> b;
+    }
+    vector<vector<int>> neighborMat;
+    vector<int> pos;
+    for (int j = 0; j < n; ++j)
+    {
+        vector<int> tmpNeighbors;
+        tmpNeighbors = getNeighbors(A[j]);
+        neighborMat.push_back(tmpNeighbors);
+    }
+    int i;
+    cout<<"Enter set size"<<endl;
+    cin>>i;
+    cout<< "Enter set values"<<endl;
+    for (int j= 0;j<i;++j){
+        int a;
+        cin>>a;
+        pos.push_back(a-1); 
+    }
+    sort(pos.begin(), pos.end());
+    for (int j = 0; j < i; ++j)
+    {
+        vector<int> tmpNeigbours;
+        tmpNeigbours = neighborMat[pos[j]];
+        int regularity = tmpNeigbours.size();
+        for (int x = 0; x < regularity; ++x)
+        {
+            vector<int>::iterator isInSet = lower_bound(pos.begin(), pos.end(), tmpNeigbours[x]);
+            if (isInSet != pos.end() && *isInSet == tmpNeigbours[x])
+            {
+                cout<<"Not Independent "<<pos[j] + 1<<" and "<< tmpNeigbours[x] + 1<<" are adjacent"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"It is an Indepenent set"<<endl;
+    return 0;
+}
--- dispersionAlgo.cpp
+++ dispersionAlgo.cpp
@@ -30,27 +30,26 @@ int main()
     inFile >> a >> b;
     while (a != -1 && b != -1)
     {
-        A[a-1][b-1] = 1;
-        A[b-1][a-1] = 1;
+        A[a - 1][b - 1] = 1;
+        A[b - 1][a - 1] = 1;
         inFile >> a >> b;
     }
     vector<vector<int>> neighborMat;
+    vector<int> pos;
     for (int j = 0; j < n; ++j)
     {
         vector<int> tmpNeighbors;
         tmpNeighbors = getNeighbors(A[j]);
         neighborMat.push_back(tmpNeighbors);
+        pos.push_back(j);
     }
+    random_shuffle(pos.begin(), pos.end());
     cout << "Enter independence number"
          << "\n";
     int i;
-    cin>>i;
-    vector<int> pos;
-    for (int j = 0; j < i; ++j)
-    {
-        pos.push_back(rand() % n);
-    }
-    sort(pos.begin(), pos.end());
+    cin >> i;
+    pos.resize(i);
+    sort(pos.begin(),pos.end());
     vector<int> croudedness;
     croudedness.resize(n);
     for (int j = 0; j < i; ++j)
@@ -73,20 +72,23 @@ int main()
     }
     int moved = 0;
     int independent = 0;
-    for (int ite = 0; ite < 3000; ++ite)
+    for (int ite = 0; ite < 9000; ++ite)
     {
-        cout<<"independent"<< " moved"<<endl;
-        cout<<independent<<"         "<< moved<<endl;
-        if (independent == i){
-            cout<< "Found set in "<< ite << " iterations. The set: \n";
-            for(int j = 0; j<i;++j){
-                cout<<pos[j]<<" ";
+        cout << "independent"
+             << " moved" << endl;
+        cout << independent << "         " << moved << endl;
+        if (independent == i)
+        {
+            cout << "Found set in " << ite << " iterations. The set: \n";
+            for (int j = 0; j < i; ++j)
+            {
+                cout << pos[j]+1 << " ";
             }
-            cout<<"\n";
-            break;
+            cout << "\n";
+            return 0;
         }
         independent = 0;
-        moved = 0; 
+        moved = 0;
         for (int j = 0; j < i; ++j)
         {
             vector<int> tmpNeigbours;
@@ -94,50 +96,80 @@ int main()
             int regularity = tmpNeigbours.size();
             int minC = -(croudedness[pos[j]]);
             int minCidx = pos[j];
-            for (int x = 0; x < regularity; ++x) {
-                if (croudedness[tmpNeigbours[x]]>=0){
-                    if(minC>croudedness[tmpNeigbours[x]]){
-                        minC = croudedness[tmpNeigbours[x]];
-                        minCidx = tmpNeigbours[x];
+            for (int x = 0; x < regularity; ++x)
+            {
+                if (croudedness[tmpNeigbours[x]] > 0)
+                {
+                    if (minC > croudedness[tmpNeigbours[x]] - 1)
+                    {
+                        minC = croudedness[tmpNeigbours[x]] - 1;
+                        minCidx = tmpNeigbours[x];   
                     }
-                    else if ( minC == croudedness[tmpNeigbours[x]]){
-                        if (rand()%2==0) {
+                    else if (minC == croudedness[tmpNeigbours[x]] - 1)
+                    {
+                        if (rand() % 2 == 0)
+                        {
                             minCidx = tmpNeigbours[x];
                         }
                     }
                 }
+                else if(croudedness[tmpNeigbours[x]] == 0){
+                    cout<<"Neigbor can't never have 0 crowdedness"<<endl;
+                    return 0;
+                }
             }
-            if (pos[j]!=minCidx){
-                for (int x = 0; x<regularity; ++x){
-                    if (minCidx==tmpNeigbours[x]){
-                        continue;
+            if (pos[j] != minCidx)
+            {
+                for (int x = 0; x < regularity; ++x)
+                { 
+                    if (croudedness[tmpNeigbours[x]] > 0)
+                    {
+                        croudedness[tmpNeigbours[x]] -= 1;
                     }
-                    if(croudedness[tmpNeigbours[x]]>0){
-                        croudedness[tmpNeigbours[x]]-=1;
-                    }
-                    else{
+                    else
+                    {
                         croudedness[tmpNeigbours[x]] += 1;
                     }
                 }
-                croudedness[pos[j]]=-croudedness[pos[j]];
+                croudedness[pos[j]] = -croudedness[pos[j]];
                 pos[j] = minCidx;
                 vector<int> tmpN;
                 tmpN = neighborMat[pos[j]];
                 croudedness[pos[j]] = -croudedness[pos[j]];
-                for (int x = 0; x<regularity; ++x){ 
-                    if(croudedness[tmpN[x]]<=0){
-                        croudedness[tmpN[x]]+=1;
-                    }
-                    else{
+                if (croudedness[pos[j]] > 0)
+                {
+                    cout << "not possible" << endl;
+                    return 0;
+                }
+                for (int x = 0; x < regularity; ++x)
+                {
+                    if (croudedness[tmpN[x]] < 0)
+                    {
                         croudedness[tmpN[x]] -= 1;
                     }
+                    else
+                    {
+                        croudedness[tmpN[x]] += 1;
+                    }
                 }
-               moved += 1; 
+                moved += 1;
             }
-            if (croudedness[pos[j]] == 0){
-                independent +=1;      
-            } 
+            if (croudedness[pos[j]] == 0)
+            {
+                independent += 1;
+            }
         }
     }
+    cout << "Didn't find set of size i but here is the current set: \n";
+    int count = 0;
+    for (int j = 0; j < i; ++j)
+    {
+        if (croudedness[pos[j]]==0){
+            cout << pos[j]+1 << ", ";
+            count += 1;
+        }
+    }
+    cout << endl;
+    cout<<count<<endl;
     return 0;
 }
